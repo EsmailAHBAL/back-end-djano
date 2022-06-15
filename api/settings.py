2cbaf4b7bc056e7f6efddcdc91c6da982274747a
EsmailAHBAL <100035479+EsmailAHBAL@users.noreply.github.com>
2022-06-15 18:53:56 +0100

    update

--- api/settings.py
+++ api/settings.py
@@ -144,7 +144,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ahbalismail10@gmail.com'
-EMAIL_HOST_PASSWORD = 'hndbosjlnynulqcy'
+EMAIL_HOST_USER = '.your email'
+EMAIL_HOST_PASSWORD = 'your password'/
 
-GOOGLE_RECAPTCHA_SECRET_KEY= '6Lf-QWogAAAAAGiiTse26r841BclkYi1PU-tC_de'
+GOOGLE_RECAPTCHA_SECRET_KEY= 'by RECAPTCHA'
